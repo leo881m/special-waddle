8f1005050cad3fe491da583446cf98adcc7226a4
leo881m <leomendes881@gmail.com>
2026-04-24 21:40:52 -0300

    Refactor test cases in test.py
    Refactor test cases to remove unnecessary yield statements and improve assertions.

--- test/test.py
+++ test/test.py
@@ -7,51 +7,44 @@ import pytest_asyncio
 
 @pytest.mark.asyncio
 async def test_root():
-    result =  await root()
-    yield result
+    result = await root()
     assert result == {"mensagem": "Hello", "mundo": "World"}
+
 
 @pytest.mark.asyncio
 async def test_funcaoteste():
     with patch("random.randint", return_value=42):
         result = await funcaoteste()
-        yield result
+        assert result == {"teste": True, "numeroAleatorio": 42}
 
-    assert result == {"teste": True, "numeroAleatorio": 42}
 
 @pytest.mark.asyncio
 async def test_create_item():
     item_teste = Item(name="Teste", preco=10.0)
     result = await create_item(item_teste)
-    assert create_item == result
+    assert result.name == "Teste"
+    assert result.preco == 10.0
+
 
 @pytest.mark.asyncio
 async def test_update_item_negativo():
-    result = await update_item(-5)   
+    result = await update_item(-5)
     assert not result
+
 
 @pytest.mark.asyncio
 async def test_update_item_positivo():
     result = await update_item(10)
-    assert result   
-
-@pytest.mark.asyncio
-async def teste_delete_item_negativo():
-    result = await delete_item(-5)
-    assert not result
-
-@pytest.mark.asyncio
-async def teste_delete_item_positivo():
-    result = await delete_item(10)
     assert result
 
-@pytest.mark.asyncio   
+
+@pytest.mark.asyncio
 async def test_delete_item_negativo():
     result = await delete_item(-5)
     assert not result
+
 
 @pytest.mark.asyncio
 async def test_delete_item_positivo():
     result = await delete_item(10)
     assert result
-    
